4964e6863be2ca1e8b3c51a17fd30733470a854c
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-26 00:52:37 +0000

    aaaaa

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ A program generáljon 10 darab véletlenszámot [1;3] intervallumon, ezeket tár
 A felhasználónak legyen lehetősége megadni egy számot [1;3] intervallumon, és a program törölje ennek a számnak valamennyi előfordulását a listából, majd írja ki a módosított listát a képernyőre!
 '''
 from random import randint
-veletlen = [randint(1,3) for szam in range(10)]
+veletlen= []
+aaa = []
+for sza in range(10):
+    szam = randint(1,3)
+    aaa.append(szam)
+    veletlen.append(szam)
 print(veletlen)
 
 beker = int(input('adj egy szamot (1-3):  '))
-for szam in veletlen:
+for szam in aaa:
     if beker == szam:
         veletlen.remove(szam)
 
